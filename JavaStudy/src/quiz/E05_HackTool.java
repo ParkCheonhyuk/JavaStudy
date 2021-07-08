ef2ee4419a972ff914021bfda64324c014e057d6
1103-22 <1103-22@host.docker.internal>
2021-07-08 21:43:08 +0900

    2021/07/08/final

--- JavaStudy/src/quiz/E05_HackTool.java
+++ JavaStudy/src/quiz/E05_HackTool.java
@@ -1,10 +1,13 @@
 package quiz;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class E05_HackTool {
 	/*
@@ -14,33 +17,26 @@ public class E05_HackTool {
 	*/
 	
 	public static void main(String[] args) {
-		decryption("frankenstein_encrypted.txt");
+		bruteforce("frankenstein_encrypted.txt");
 	}
 	
-	public static void decryption(String path) {
-		File file = new File(path);
-		try{
-			
-			for(int key = 1; key <= 25; key++) {
-				
-				FileReader fin = new FileReader(file);
+	public static void bruteforce(String path) {
+		File target = new File(path);
+		ArrayList<String> lines = new ArrayList<>();
+		try(
+				FileReader fin = new FileReader(target);
 				BufferedReader in = new BufferedReader(fin);
-				System.out.println("------------------[key:"+key+"]------------------");
+		){
+				// 10번 또는 파일이 끝날때까지 반복
 				String line;
-				for(int j = 0; j < 10; j++) {
-					line = in.readLine();
-					for(char ch : line.toCharArray()) {
-						if(Character.isUpperCase(ch)) {
-							ch = (char)((ch- 'A' + key) % 26 +'A');
-						}else if(Character.isLowerCase(ch)) {
-							ch = (char)((ch- 'a' + key) % 26 +'a');
-						}
-						System.out.print(ch);
+				for(int i = 0; i <10 && (line = in.readLine())!=null; ++i) {
+					// 줄이 비어있거나 문자가 5개도 안되는 줄은 안본걸로 치겠다
+					if(line.trim().equals("") || line.length() <5) {
+						--i;
+					} else {
+						lines.add(line);
 					}
-					System.out.println();
 				}
-				in.close();
-			}
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -48,5 +44,31 @@ public class E05_HackTool {
 			e.printStackTrace();
 		}
 		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int key = 1; key< 26; ++key) {
+			System.out.printf("----- [Key: %d] --------------------\n",key);
+			for(String line : lines) {
+				for(char ch : line.toCharArray()) {
+					if(Character.isUpperCase(ch)) {
+						ch = (char)(ch - key < 'A' ? ch - key + 26 : ch - key);
+					}else if(Character.isLowerCase(ch)) {
+						ch = (char)(ch - key < 'a' ? ch - key + 26 : ch - key);
+					}
+					System.out.print(ch);
+				}
+				System.out.println();
+			}
+			System.out.println();
+			
+			System.out.println("Press 'Enter' to continue...");
+			String cmd = sc.nextLine();
+			
+			if(cmd.toLowerCase().equals("d")) {
+				System.out.println("복호화를 진행합니다..");
+				break;
+			}
+		}
+		System.out.println("프로그램 끝");
 	}
 }
